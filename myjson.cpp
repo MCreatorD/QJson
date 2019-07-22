d9ece109ee7609ede7a8bba273a8e02f7373fe27
wang <2854836020@qq.com>
2019-07-22 17:43:05 +0800

    Signed-off-by: wang <2854836020@qq.com>

--- myjson.cpp
+++ myjson.cpp
@@ -5,57 +5,91 @@
 #include <QFile>
 #include <QDir>
 #include <QStringList>
+
 MyJson::MyJson(QWidget *parent) : QWidget(parent)
 {
-    //定义 JSON 的测试
-    QJsonObject obj;
-    obj.insert("user_name",QString ("Jwang"));
-    obj.insert("passwd",QString("qwe12315"));
-
-    QJsonDocument doc(obj);
-
-    qDebug()<<doc.toJson();
-
-
-    // 以读写方式打开主目录下的1.json文件，若该文件不存在则会自动创建
-    QFile file(QDir::homePath() + "/2.json");
+    //依据JSON文件加载对话框初始值
+    //1.打开JSON 文件
+    m_jsonfile = new QFile(QCoreApplication::applicationDirPath() + "/init.json");
     //qDebug()<<QDir::homePath();
 
-    if(!file.open(QIODevice::ReadWrite)) {
+    if(!m_jsonfile->open(QIODevice::ReadWrite)) {
         qDebug() << "File open error";
         //return -1;
     } else {
         qDebug() << "File open!";
     }
 
-    QByteArray allData = file.readAll();
-    file.close();
+    QByteArray jsonfileData = m_jsonfile->readAll();
+    m_jsonfile->close();
 
-    QJsonParseError json_error;
-    QJsonDocument jsonDoc(QJsonDocument::fromJson(allData, &json_error));
+    //2. 文件内容读入到JSON Document
+    QJsonParseError jsonfile_error;
+    m_jsondoc = new QJsonDocument(QJsonDocument::fromJson(jsonfileData, &jsonfile_error));
 
-    if(json_error.error != QJsonParseError::NoError)
+
+    if(jsonfile_error.error != QJsonParseError::NoError)
     {
-        qDebug("json error[%d]!",json_error.error);
+        qDebug("json error[%d]!",jsonfile_error.error);
         //return;
     }
 
-    QJsonObject rootObj = jsonDoc.object();
+    //3. JSON Document 转化成JSON 对象
+    m_jsonobj = m_jsondoc->object();
+    //JSON 对象读操作
+    QString s_power = m_jsonobj.value("功率").toString();
+    QString s_fre   = m_jsonobj.value("频率").toString();
+    QString s_mode  = m_jsonobj.value("模式").toString();
 
-    QStringList keys = rootObj.keys();
-    for(int i = 0; i < keys.size(); i++)
-    {
-        qDebug() << "key" << i << " is:" << keys.at(i);
-        qDebug() << rootObj.value(QString(keys.at(i)));
+    qDebug()<<s_power;
+    qDebug()<<s_fre;
+    qDebug()<<s_mode;
+
+     m_powerlabel = new QLabel(this);
+     m_powerlabel->setText("功率");
+     m_powerlabel->move(20,24);
+
+     m_poweredit = new QLineEdit(this);
+     m_poweredit->setText(s_power);
+     m_poweredit->move(50,20);
+
+     m_frelabel = new QLabel(this);
+     m_frelabel->setText("频率");
+     m_frelabel->move(20,54);
+
+     m_freedit = new QLineEdit(this);
+     m_freedit->setText(s_fre);
+     m_freedit->move(50,50);
+
+     m_btn=new QPushButton(this);
+     m_btn->setText("保存配置");
+     m_btn->move(20,100);
+
+     connect(m_btn,&QPushButton::clicked,this,&MyJson::savejson);
+}
+
+void MyJson::savejson()
+{
+    if(!m_jsonfile->open(QIODevice::ReadWrite)) {
+        qDebug() << "File open error";
+        //return -1;
+    } else {
+        qDebug() << "File open!";
     }
 
+    //JSON 对象写操作
+    //1.    读取界面的数值
+    qDebug()<< m_poweredit->text();
+    qDebug()<< m_freedit->text();
+    //2.    将数据保存到JSON文件
+    m_jsonobj.insert("功率",QString (m_poweredit->text()));
+    m_jsonobj.insert("频率",QString (m_freedit->text()));
 
-//rootObj
+    //3.    obj 转 Doc
+    QJsonDocument doc(m_jsonobj);
 
-//    // 将json以文本形式写入文件并关闭文件。
-//    file.write(doc.toJson());
-//    file.close();
-
-//    qDebug() << "Write to file";
-
+    //4.    Doc转Json格式文件存储
+    qDebug()<<doc.toJson();
+    m_jsonfile->write(doc.toJson());
+    m_jsonfile->close();
 }
--- myjson.h
+++ myjson.h
@@ -2,6 +2,14 @@
 #define MYJSON_H
 
 #include <QWidget>
+#include <QFile>
+#include <QJsonObject>
+#include <QJsonDocument>
+
+#include <QLabel>
+#include <QLineEdit>
+#include <QCoreApplication>
+#include <QPushButton>
 
 class MyJson : public QWidget
 {
@@ -12,6 +20,18 @@ public:
 signals:
 
 public slots:
+    void savejson();
+private:
+    QFile *m_jsonfile;//操作的文件
+
+    QJsonDocument *m_jsondoc;//JSON文件
+    QJsonObject m_jsonobj;//JSON对象
+private://界面
+    QLabel      *m_powerlabel;
+    QLineEdit   *m_poweredit;
+    QLabel      *m_frelabel;
+    QLineEdit   *m_freedit;
+    QPushButton *m_btn;
 };
 
 #endif // MYJSON_H
